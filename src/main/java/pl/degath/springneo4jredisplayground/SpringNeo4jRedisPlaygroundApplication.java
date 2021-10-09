f4512b339def4d2f7efe8e747ba40198489b9dbe
degath <zaleskid1@gmail.com>
2021-10-09 20:06:43 +0200

    Initial commit

---
+++ src/main/java/pl/degath/springneo4jredisplayground/SpringNeo4jRedisPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package pl.degath.springneo4jredisplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNeo4jRedisPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNeo4jRedisPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/degath/springneo4jredisplayground/SpringNeo4jRedisPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.degath.springneo4jredisplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNeo4jRedisPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
